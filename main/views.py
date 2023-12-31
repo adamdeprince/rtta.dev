5a6b69e0d18718dc3a7b990b5be55854adaafb0a
Adam DePrince <adam@adamdeprince.me>
2023-12-31 13:13:15 -0500

    Checked spelling; moved templates

--- main/views.py
+++ main/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request, 'index.html')
+    return render(request, 'main/index.html')
 
 def install(request):
-    return render(request, 'install.html')
+    return render(request, 'main/install.html')
 
 def overview(request):
-    return render(request, "overview.html")
+    return render(request, "main/overview.html")
